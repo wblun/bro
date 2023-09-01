9c048d234dd938966e011f00340cdccbf885d956
wblun <Wonder.blunder@gmail.com>
2023-09-01 17:46:24 +0530

    My change

---
+++ src/test/java/dem/NewTest.java
@@ -0,0 +1,11 @@
+package dem;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class NewTest {
+  @Test
+  public void f() {
+	  Assert.assertEquals(false, false);
+  }
+}
